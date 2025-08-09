b16627ad1fc873c092aeaea69458dce21e586a46
gauravdaksh877 <gauravdaksh877@gmail.com>
2025-08-09 16:56:45 +0530

    Initial Commit

---
+++ IOSExchange/IOSExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSExchangeApp.swift
+//  IOSExchange
+//
+//  Created by Gaurav Kumar on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct IOSExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOSExchangeTests/IOSExchangeTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOSExchangeTests.swift
+//  IOSExchangeTests
+//
+//  Created by Gaurav Kumar on 09/08/25.
+//
+
+import Testing
+@testable import IOSExchange
+
+struct IOSExchangeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
